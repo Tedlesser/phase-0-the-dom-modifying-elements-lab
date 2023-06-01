749d57d43dce080ffb2b55e5a813a1e8fc5967b5
Theodore Lesser <ted.a.lesser@gmail.com>
2023-06-01 10:50:23 -0400

    Adds myFile

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code here!
+// Write your code here 
+main. remove ();
+const newHeader = document.createElement("h1");
+const yourName = 'Theodore Lesser';
+newHeader.setAttribute('id','victory');
+newHeader.innerHTML = `${yourName} is the champion`
+
+
+
